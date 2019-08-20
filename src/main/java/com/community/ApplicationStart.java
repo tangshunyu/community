06c1439d0a5b0caec208cd46b05c935632466cd3
tom_glb <goodmoringglb@163.com>
2019-08-20 23:15:54 +0800

    second

---
+++ src/main/java/com/community/ApplicationStart.java
@@ -0,0 +1,17 @@
+package com.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: tangshunyu
+ * @Date: 2019/8/20
+ * @Description:
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class,args);
+    }
+}
